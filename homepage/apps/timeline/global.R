a8fa13e44e43704182e46c6cefebf3fc28580f09
JuliaS92 <julia@schessner-home.de>
2013-08-26 11:50:18 +0200

    adjusted server and ui

--- homepage/apps/timeline/global.R
+++ homepage/apps/timeline/global.R
@@ -13,20 +13,17 @@ dat$region <- gsub("Americas East", "America", dat$region)
 dat$region <- gsub("Americas West", "America", dat$region)
 dat$region <- gsub("Latin America", "America", dat$region)
 dat$region <- gsub("Americas", "America", dat$region)
-# temporary hack, but don't want "specify region" to appear in charts :P
-dat = dat[dat$region != "--Specify Region--",]
+dat$region <- gsub("--Specify Region--", "No region specified", dat$region)
 # temporary hack, but remove the 1 african team
 #dat = dat[dat$region != "Africa",]
 
 ## Layout choice lists:
 myChoicesForX = c("Students", "Teams", "Instructors", "Advisors", "ChampionshipAwards")
-#myChoicesForRegion <- levels(as.factor(DATParametersFromJSON$region))
-myChoicesForRegion <- c("Europe", "Asia", "America", "Africa")
-
+myChoicesForRegion <- levels(as.factor(dat$region))
 myChoicesForTrack <- levels(as.factor(DATParametersFromJSON$track))
 myChoicesForScore <- c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
-#myChoicesForRegional_awards <- levels(as.factor(DATContentsFromJSON$awards_regional))
-#myChoicesForChampionship_awards <- levels(as.factor(DATContentsFromJSON$awards_championship))
+myChoicesForRegional_awards <- levels(as.factor(DATContentsFromJSON$awards_regional))
+myChoicesForChampionship_awards <- levels(as.factor(DATContentsFromJSON$awards_championship))
 myChoicesForBB_count <- c(0, 5, 10, 20, 50, 100, 200, ">200")
 myChoicesForStudents_count <- c(0, 5, 10, 15, 20, ">20")
 myChoicesForStudents_count <- c(0, 5, 10, 15, 20, ">20")
@@ -34,82 +31,109 @@ myChoicesForAdvisors_count <- c(0, 2, 5, 10, 15, ">15")
 myChoicesForInstructors_count <- c(0, 2, 5, 10, 15, ">15")
 
 # big ass filter
-bbqSauceFilter <- function(data, input){
-    data <- FilterForRegion(data, input)
-    data <- FilterForTrack(data, input)
-	#data <- FilterForScore(data, input)
-	#data <- FilterForBiobrick_count(data, input)
-	#data <- FilterForStudents_count(data, input)
-	#data <- FilterForAdvisors_count(data, input)
-	#data <- FilterForInstructors_count(data, input)
+bbqSauceFilter <- function(data){
+    data <- FilterForRegion(data)
+    data <- FilterForTrack(data)
+	data <- FilterForScore(data)
+	data <- FilterForBiobrick_count(data)
+	data <- FilterForStudents_count(data)
+	data <- FilterForAdvisors_count(data)
+	data <- FilterForInstructors_count(data)
+	return(data)
 }
-# General filters:
-FilterForRegion <- function(data, input) {
+FilterForRegion <- function(data) {
 	matchRegion <- rep(0, times=length(data$region))
 	for (i in 1:length(input$FILregion)) {
 		matchRegion[which(data$region == input$FILregion[i])] <- 1
 	}
-	data <- data[which(matchRegion == 1),]
+	delete <- which(matchRegion == 0)
+	if (length(delete) != 0) data <- data[-delete,]
 	rm(matchRegion)
+	rm(delete)
 	return(data)
 }
-FilterForTrack <- function(data, input) {
+FilterForTrack <- function(data) {
 	matchTrack <- rep(0, times=length(data$track))
 	for (i in 1:length(input$FILtrack)) {
 		matchTrack[which(data$track == input$FILtrack[i])] <- 1
 	}
-	data <- data[which(matchTrack == 1),]
+	delete <- which(matchTrack == 0)
+	if (length(delete) != 0) data <- data[-delete,]
 	rm(matchTrack)
+	rm(delete)
 	return(data)
 }
-
-# Success filters:
-FilterForScore <- function(data, input) {
-	data <- data[-which(data$score < input$FILscore_min | data$score > input$FILscore_max),]
-	return(data)
-}
-FilterForBiobrick_count <- function(data, input) {
-	if (input$FILbiobrick_count_min == ">200") data <- data[-which(data$biobrick_count < 200),]
-	else if (input$FILbiobrick_count_max == ">200") data <- data[-which(data$biobrick_count < input$FILbiobrick_count_min),]
-	else data <- data[-which(data$biobrick_count < input$FILbiobrick_count_min | data$biobrick_count > input$FILbiobrick_count_max),]
-	return(data)
-}
-#FilterForRegionalAwards <- function(data) {
-#	matchRegionalAw <- rep(0, times=length(data$awards_regional))
-#	for (i in 1:length(input$FILawards_regional)) {
-#		matchRegionalAw[which(data$awards_regional == input$FILawards_regional[i])] <- 1
-#	}
-#	data <- data[-which(matchRegionalAw == 0),]
-#	rm(matchRegionalAw)
-#	return(data)
-#}
-#FilterForChampionshipAwards <- function(data) {
-#	matchChampionshipAw <- rep(0, times=length(data$awards_championship))
-#	for (i in 1:length(input$FILawards_championship)) {
-#		matchChampionshipAw[which(data$awards_championship == input$FILawards_championship[i])] <- 1
-#	}
-#	data <- data[-which(matchChampionshipAw == 0),]
-#	rm(matchChampionshipAw)
-#	return(data)
-#}
-
-# Composition filters:
-
-FilterForStudents_count <- function(data, input) {
+FilterForStudents_count <- function(data) {
 	if (input$FILstudents_count_min == ">20") data <- data[-which(data$students_count < 20),]
-	else if (input$FILstudents_count_max == ">20") data <- data[-which(data$students_count < input$FILstudents_count_min),]
-	else data <- data[-which(data$students_count < input$FILstudents_count_min | data$students_count > input$FILstudents_count_max),]
+	else if (input$FILstudents_count_max == ">20" & input$FILstudents_count_min != "0") data <- data[-which(data$students_count < as.numeric(input$FILstudents_count_min)),]
+	else if (input$FILstudents_count_max == ">20" & input$FILstudents_count_min == "0") return(data)
+	else data <- data[-which(data$students_count < as.numeric(input$FILstudents_count_min) | data$students_count > as.numeric(input$FILstudents_count_max)),]
 	return(data)
 }
-FilterForAdvisors_count <- function(data, input) {
+FilterForAdvisors_count <- function(data) {
 	if (input$FILadvisors_count_min == ">15") data <- data[-which(data$advisors_count < 15),]
-	else if (input$FILadvisors_count_max == ">15") data <- data[-which(data$advisors_count < input$FILadvisors_count_min),]
-	else data <- data[-which(data$advisors_count < input$FILadvisors_count_min | data$advisors_count > input$FILadvisors_count_max),]
+	else if (input$FILadvisors_count_max == ">15" & input$FILadvisors_count_min != "0") data <- data[-which(data$advisors_count < as.numeric(input$FILadvisors_count_min)),]
+	else if (input$FILadvisors_count_max == ">15" & input$FILadvisors_count_min == "0") return(data)
+	else data <- data[-which(data$advisors_count < as.numeric(input$FILadvisors_count_min) | data$advisors_count > as.numeric(input$FILadvisors_count_max)),]
 	return(data)
 }
-FilterForInstructors_count <- function(data, input) {
+FilterForInstructors_count <- function(data) {
 	if (input$FILinstructors_count_min == ">15") data <- data[-which(data$instructors_count < 15),]
-	else if (input$FILinstructors_count_max == ">15") data <- data[-which(data$instructors_count < input$FILinstructors_count_min),]
-	else data <- data[-which(data$instructors_count < input$FILinstructors_count_min | data$instructors_count > input$FILinstructors_count_max),]
+	else if (input$FILinstructors_count_max == ">15" & input$FILinstructors_count_min != "0") data <- data[-which(data$instructors_count < as.numeric(input$FILinstructors_count_min)),]
+	else if (input$FILinstructors_count_max == ">15" & input$FILinstructors_count_min == "0") return(data)
+	else data <- data[-which(data$instructors_count < as.numeric(input$FILinstructors_count_min) | data$instructors_count > as.numeric(input$FILinstructors_count_max)),]
+	return(data)
+}
+FilterForScore <- function(data) {
+	delete <- which(data$score < input$FILscore_min | data$score > input$FILscore_max)
+	if (length(delete) != 0) data <- data[-delete,]
+	rm(delete)
+	return(data)
+}
+FilterForBiobrick_count <- function(data) {
+	if (input$FILbiobrick_count_min == ">200") data <- data[-which(data$biobrick_count < 200),]
+	else if (input$FILbiobrick_count_max == ">200" & input$FILbiobrick_count_min != "0") data <- data[-which(data$biobrick_count < as.numeric(input$FILbiobrick_count_min)),]
+	else if (input$FILbiobrick_count_max == ">200" & input$FILbiobrick_count_min == "0") return(data)
+	else data <- data[-which(data$biobrick_count < as.numeric(input$FILbiobrick_count_min) | data$biobrick_count > as.numeric(input$FILbiobrick_count_max)),]
+	return(data)
+}
+FilterForRegionalAwards <- function(data) {
+	keepteams <- c()
+	Contents <- DATContentsFromJSON
+	for (i in 1:length(input$FILawards_regional)) {
+		deleteindex <- c()
+		for (j in 1:length(names(Contents))) {
+			if (length(grep(input$FILawards_regional[i], Contents[[j]]$awards_regional)) != 0) {
+				keepteams <- c(keepteams, names(Contents)[j])
+				deleteindex <- c(deleteindex, j)
+			}
+		}
+		Contents <- Contents[-deleteindex]
+	}
+	if (length(keepteams) != 0) data <- data[keepteams,]
+	if (length(grep("NA", row.names(data))) != 0 ) data <- data[-grep("NA", row.names(data)),]
+	rm(keepteams)
+	rm(Contents)
+	rm(deleteindex)
+	return(data)
+}
+FilterForChampionshipAwards <- function(data) {
+	keepteams <- c()
+	Contents <- DATContentsFromJSON
+	for (i in 1:length(input$FILawards_championship)) {
+		deleteindex <- c()
+		for (j in 1:length(names(Contents))) {
+			if (length(grep(input$FILawards_championship[i], Contents[[j]]$awards_championship)) != 0) {
+				keepteams <- c(keepteams, names(Contents)[j])
+				deleteindex <- c(deleteindex, j)
+			}
+		}
+		Contents <- Contents[-deleteindex]
+	}
+	if (length(keepteams) != 0) data <- data[keepteams,]
+	if (length(grep("NA", row.names(data))) != 0 ) data <- data[-grep("NA", row.names(data)),]
+	rm(keepteams)
+	rm(Contents)
+	rm(deleteindex)
 	return(data)
 }
--- homepage/apps/timeline/server.R
+++ homepage/apps/timeline/server.R
@@ -1,28 +1,6 @@
 shinyServer(function(input, output) {
 ## get all stuff in global up and running
 source("global.R")
-## Filter data:
-
-#reactive(quote(dat <- FilterForScore(dat)))
-#reactive(quote(dat <- FilterForRegionalAwards(dat)))
-#reactive(quote(dar <- FilterForChampionshipAwards(dat)))
-#reactive(quote(dat <- FilterForBiobrick_count(dat)))
-
-#filter <- reactive(quote(dat <- FilterForStudents_count(dat)))
-#reactive(quote(dat <- FilterForAdvisors_count(dat)))
-#reactive(quote(dat <- FilterForInstructors_count(dat)))
-
-## Put regions together:
-#dat$region <- gsub("Canada", "America", dat$region)
-#dat$region <- gsub("US", "America", dat$region)
-#dat$region <- gsub("Americas East", "America", dat$region)
-#dat$region <- gsub("Americas West", "America", dat$region)
-#dat$region <- gsub("Latin America", "America", dat$region)
-#dat$region <- gsub("Americas", "America", dat$region)
-# temporary hack, but don't want "specify region" to appear in charts :P
-#dat = dat[dat$region != "--Specify Region--",]
-# temporary hack, but remove the 1 african team
-#dat = dat[dat$region != "Africa",]
 
 # big function, which takes as input the split data.frames by ddply and returns different summary statistics
 timelineDatGenerator <- function(x) {
@@ -50,7 +28,7 @@ timelineNvd3Gen <- function(x) {
 }
 
 # ok time for some reactive ACTION
-dat2 <- reactive({bbqSauceFilter(dat, input)})
+dat2 <- reactive({bbqSauceFilter(dat)})
 timelineDat <- reactive({timelineNvd3Gen(dat2())})
 
 
